2f1051dbe14d344e3a3fd15349cfc9c47000dcc8
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-11-10 20:57:23 +0900

    sign

---
+++ GaziGara/app/src/main/java/com/example/quote/gazigara/Sign.kt
@@ -0,0 +1,12 @@
+package com.example.quote.gazigara
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
+    }
+}
